976f65c7d5c642befeed89312515048ddb5e7cd3
Igopogo <hieu@autonomous.nyc>
2018-05-24 14:49:44 +0700

    add formik

--- src/pages/Create/components/CreateSeedItem/CreateSeedItem.jsx
+++ src/pages/Create/components/CreateSeedItem/CreateSeedItem.jsx
@@ -1,59 +1,61 @@
 import React from 'react';
-import { Formik } from 'formik';
+import {Formik} from 'formik';
 
 class CreateSeedItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+  }
+
+  onSubmit(values, {setSubmitting, setErrors /* setValues and other goodies */}) {
+    console.log("here");
+  }
+
+  validateForm(values) {
+    // same as above, but feel free to move this into a class method now.
+    let errors = {};
+    return errors;
+  }
+
+  renderForm({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting}) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email}
+        />
+        {touched.email && errors.email && <div>{errors.email}</div>}
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+        />
+        {touched.password && errors.password && <div>{errors.password}</div>}
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+
   render() {
     return (
-      <div>
-        <h1>My Form</h1>
-        <p>This can be anywhere in your application</p>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          validate={values => {
-            // same as above, but feel free to move this into a class method now.
-            let errors = {};
-
-            return errors;
-          }}
-          onSubmit={(values, {setSubmitting, setErrors /* setValues and other goodies */}) => {
-            console.log("here");
-          }}
-          render={({
-                     values,
-                     errors,
-                     touched,
-                     handleChange,
-                     handleBlur,
-                     handleSubmit,
-                     isSubmitting,
-                   }) => (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-              {touched.email && errors.email && <div>{errors.email}</div>}
-              <input
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              />
-              {touched.password && errors.password && <div>{errors.password}</div>}
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </form>
-          )}
-        />
-      </div>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validate={this.validateForm}
+        onSubmit={this.onSubmit}
+        render={this.renderForm}
+      />
     );
   }
 }
