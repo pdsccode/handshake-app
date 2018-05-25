163b57f1cae8cca0dcc71729384877353d497bdc
0xngoubou <Phuong_Mac@Phuong-MacBook-Pro.local>
2018-05-26 01:50:52 +0700

    add Bitcoin model: check balance

---
+++ src/models/Bitcoin.1.js
@@ -0,0 +1,23 @@
+
+import axios from 'axios'
+import satoshi from 'satoshi-bitcoin';
+import { rule } from 'postcss';
+export class Bitcoin {      
+    
+    static Network = {"Mainnet": 'https://insight.bitpay.com/api', "Testnet": "https://test-insight.bitpay.com/api"}    
+
+    constructor(ethereumNetwork) {   
+      this.ethereumNetwork = ethereumNetwork;               
+    }
+      
+    async getBalance(address) {      
+      var url = this.ethereumNetwork + '/addr/' + address + '/balance';        
+      console.log(url);
+      var response = await axios.get(url);      
+      // console.log(response.data);
+      if (response.status == 200){
+        return satoshi.toBitcoin(response.data);
+      }
+      return false;      
+    }
+}
