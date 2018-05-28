16b2c623c7281b82605e20d563349aaf35a64cf9
0xtrunko <nhan@autonomous.nyc>
2018-05-28 11:31:14 +0700

    refine UI

--- src/components/handshakes/exchange/Feed/ExchangeFeed.jsx
+++ src/components/handshakes/exchange/Feed/ExchangeFeed.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import iconLocation from '@/assets/images/icon/icons8-geo_fence.svg';
 // style
 import './ExchangeFeed.scss';
 import {FormattedMessage} from 'react-intl';
@@ -10,23 +11,28 @@ import {AMOUNT_DECIMAL, PRICE_DECIMAL} from "@/constants";
 
 
 class ExchangeFeed extends React.PureComponent {
-  BACKGROUND_COLORS = ['#0064ff','#ff00a2','#ff9b00', '#00ce7d'];
-
-  backgroundCss(background) {
-    return background ? background : this.BACKGROUND_COLORS[Math.floor(Math.random() * this.BACKGROUND_COLORS.length)];
-  }
-
   render() {
     const {type, amount, currency, fiat_amount, ...props } = this.props;
     return (
-      <div>
-        <Feed className="feed">
-          <p className="description"><FormattedMessage id="offerHandShakeContent" values={{ offerType: type === 'buy' ? 'Buy': 'Sell',
+      <div className="feed-wrapper">
+        <Feed className="feed p-2 text-white" background="#FF2D55">
+          <h5>
+            <FormattedMessage id="offerHandShakeContent" values={{ offerType: type === 'buy' ? 'Buy': 'Sell',
             amount: new BigNumber(amount).toFormat(AMOUNT_DECIMAL), currency: currency, total: new BigNumber(fiat_amount).toFormat(PRICE_DECIMAL)
-          }} /></p>
-          <p className="email"></p>
-          <p className="email"><FormattedMessage id="offerDistanceContent" values={{ offerType: type === 'buy' ? 'Buyer': 'Seller',
-            distance: 100}} /></p>
+            }}/>
+          </h5>
+          <div className="media">
+            <img className="mr-1" src={iconLocation} width={22} />
+              <div className="media-body">
+                <h6 className="mt-0">81 E.Augusta Ave. Salinas</h6>
+                <div>
+                  <small>
+                    <FormattedMessage id="offerDistanceContent" values={{ offerType: type === 'buy' ? 'Buyer': 'Seller',
+                      distance: 100}} />
+                  </small>
+                </div>
+              </div>
+          </div>
         </Feed>
         <Button block>Shake now</Button>
       </div>
