545def3f25564311445e2e7864d416fe66d45d75
Unknown <tam@autonomous.nyc>
2018-05-28 11:25:02 +0700

    update UI some component.

--- src/components/core/presentation/Feed/Feed.jsx
+++ src/components/core/presentation/Feed/Feed.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './Feed.scss';
 
 class Feed extends React.PureComponent {
-  BACKGROUND_COLORS = ['#0064ff','#ff00a2','#ff9b00', '#00ce7d'];
+  BACKGROUND_COLORS = ['#007AFF', '#5AC8FA', '#009688', '#FF9500', '#FF2D55', '#4CD964', '#9C27B0', '#FF3B30', '#5677FC', '#FF5722'];
 
   backgroundCss(background) {
     return background ? background : this.BACKGROUND_COLORS[Math.floor(Math.random() * this.BACKGROUND_COLORS.length)];
