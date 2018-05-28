279f1c9b94db42e39b2528c14c18fa7e00e13b5a
Unknown <tam@autonomous.nyc>
2018-05-28 17:28:18 +0700

    update search bar component

--- src/components/core/controls/SearchBar/SearchBar.jsx
+++ src/components/core/controls/SearchBar/SearchBar.jsx
@@ -3,40 +3,31 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+// components
 import Autosuggest from 'react-autosuggest';
 // style
-import './SearchBar.scss';
 import SEARCH_ICON_SVG from '@/assets/images/icon/ic_search.svg';
+import SEARCH_CLOSE_SVG from '@/assets/images/icon/search-close.svg';
+import './SearchBar.scss';
 
 class SearchBar extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      products: [],
       value: '',
-      suggestions: [],
-      dirty: false,
+      suggestions: props.suggestions,
+      isShow: false,
     };
+    // bind
     this.handleInputSearchChange = this.handleInputSearchChange.bind(this);
-    this.getSuggestions = this.getSuggestions.bind(this);
     this.getSuggestionValue = this.getSuggestionValue.bind(this);
     this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
     this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
+    this.onInputFocus = this.onInputFocus.bind(this);
+    this.resetSearch = this.resetSearch.bind(this);
+    this.getSuggestions = this.getSuggestions.bind(this);
     this.onSuggestionSelected = this.onSuggestionSelected.bind(this);
-    this.onInputClick = this.onInputClick.bind(this);
   }
-  // variable
-  theme = user => ({
-    container: 'search-bar-container',
-    input: '',
-    suggestionsList: `list-group`,
-    suggestion: `list-group-item`,
-    suggestionHighlighted: '',
-    inputFocused: '',
-    containerOpen: '',
-    inputOpen: '',
-  });
 
   set value(value) {
     this.setState({
@@ -50,26 +41,12 @@ class SearchBar extends React.PureComponent {
     });
   }
 
-  set dirty(flag) {
-    this.setState({
-      dirty: flag,
-    });
-  }
-
-  get allowShowResult() {
-    return (
-      this.state.suggestions.length > 0 &&
-      this.state.dirty &&
-      this.state.value !== ''
-    );
-  }
-
   getSuggestionValue(suggestion) {
-    return suggestion.product_name;
+    return suggestion.name;
   }
 
   renderSuggestion(suggestion) {
-    return <span className={s.textBody}>{suggestion.product_name}</span>;
+    return <span className="">{suggestion.name}</span>;
   }
 
   handleInputSearchChange(event, { newValue, method }) {
@@ -77,124 +54,115 @@ class SearchBar extends React.PureComponent {
   }
 
   getSuggestions(value) {
-    const suggestions = [];
-    for (const i of this.state.products) {
-      if (
-        i.product_name.toLowerCase().indexOf(value.toLowerCase()) > -1 ||
-        i.slug.toLowerCase().indexOf(value.toLowerCase()) > -1 ||
-        JSON.stringify(i)
-          .toLowerCase()
-          .indexOf(value.toLowerCase()) > -1
-      ) {
-        suggestions.push(i);
-        if (suggestions.length >= 5) {
-          return suggestions;
-        }
-      }
-    }
-    return suggestions;
+    const { suggestions } = this.props;
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+  
+    return inputLength === 0 ? suggestions : suggestions.filter(item =>
+      item.name.toLowerCase().slice(0, inputLength) === inputValue
+    );
   }
 
   onSuggestionsFetchRequested({ value }) {
-    this.suggestions = this.getSuggestions(value);
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
   }
 
   onSuggestionsClearRequested() {
-    this.suggestions = [];
+    this.setState({
+      isShow: false
+    });
   }
 
-  onSuggestionSelected(event, data) {
-    if (typeof window !== 'undefined') {
-      window.location = `/?slug=${data.suggestion.slug}&keyword=${
-        data.suggestionValue
-      }`;
-      ga(
-        'send',
-        'event',
-        'Header',
-        'Search product in search bar',
-        `${data.suggestionValue}: ${this.state.value}`,
-      );
-    }
+  onInputFocus() {
+    this.setState({
+      isShow: true,
+      suggestions: this.props.suggestions
+    });
   }
 
-  onInputClick() {
+  resetSearch() {
+    this.value = '';
   }
 
-  componentWillMount() {
-    try {
-      const keyword = ''; // getValueParamURLQueryByName('keyword');
-      if (keyword) {
-        this.value = keyword;
-      }
-    } catch (e) {}
-  }
-
-  componentDidMount() {
+  onSuggestionSelected(e, { suggestion }) {
+    this.props.onSuggestionSelected({ suggestion });
+    this.setState({
+      isShow: false
+    });
   }
 
   renderInputComponent = inputProps => (
     <div className="input-container">
-      <img className="icon-search" src={SEARCH_ICON_SVG} />
+      <img className="icon-search" src={SEARCH_ICON_SVG} alt="search" />
       <input
         className="search-input"
-        onClick={this.onInputClick}
         {...inputProps}
       />
+      {
+        this.state.value.length > 0 && (<img onClick={this.resetSearch} className="icon-close" src={SEARCH_CLOSE_SVG} alt="close" />)
+      }
     </div>
   );
   
   render() {
-    const { value, suggestions } = this.state;
-    const { user } = this.props;
-
+    const {
+      className,
+      placeholder,
+      getSuggestionValue,
+      onSuggestionsFetchRequested,
+      onSuggestionsClearRequested,
+    } = this.props;
+    const { value, suggestions, isShow } = this.state;
     const inputProps = {
-      placeholder: 'Search',
+      placeholder,
       value,
       className: 'search-input',
       onChange: this.handleInputSearchChange,
+      onFocus: this.onInputFocus,
     };
-
     const theme = {
-      container: 'search-bar-container',
+      container: `search-bar ${className || ''}`,
       input: ``,
-      suggestionsList: `list-group`,
-      suggestion: `list-group-item`,
+      suggestionsList: `search-list-group`,
+      suggestion: `search-list-group-item`,
       suggestionHighlighted: '',
       inputFocused: '',
       containerOpen: '',
       inputOpen: '',
     };
+
     return (
       <Autosuggest
         theme={theme}
         suggestions={suggestions}
-        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-        getSuggestionValue={this.getSuggestionValue}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested || this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={onSuggestionsClearRequested || this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue || this.getSuggestionValue}
         renderSuggestion={this.renderSuggestion}
         onSuggestionSelected={this.onSuggestionSelected}
         inputProps={inputProps}
         renderInputComponent={this.renderInputComponent}
-        highlightFirstSuggestion
-        focusInputOnSuggestionClick
-        alwaysRenderSuggestions
+        alwaysRenderSuggestions={isShow}
       />
     );
   }
 }
 
 SearchBar.propTypes = {
-  productList: PropTypes.array,
-  user: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  suggestions: PropTypes.array,
+  placeholder: PropTypes.string,
+  onSuggestionSelected: PropTypes.func.isRequired,
+  onSuggestionsFetchRequested: PropTypes.func,
+  onSuggestionsClearRequested: PropTypes.func,
+  getSuggestionValue: PropTypes.func
 };
 
 SearchBar.defaultProps = {
-  user: { status: 1 },
+  suggestions: [],
+  placeholder: 'Search'
 };
 
-const mapState = state => ({
-  user: state.user,
-});
-
-export default connect(mapState)(SearchBar);
+export default SearchBar;
