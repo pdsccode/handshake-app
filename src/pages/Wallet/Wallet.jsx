80dccc5ad212dc181eb920a79d9f6aa19d364275
0xngoubou <Phuong_Mac@Phuong-MacBook-Pro.local>
2018-05-31 11:59:24 +0700

    remove icon not use

--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -13,7 +13,7 @@ import Input from '@/components/core/forms/Input/Input';
 import dontIcon from '@/assets/images/icon/3-dot-icon.svg';
 import iconSafe from '@/assets/images/icon/icon-safe.svg';
 import iconWarning from '@/assets/images/icon/icon-warning.svg';
-import iconChecked from '@/assets/images/icon/icon-check.png';
+// import iconChecked from '@/assets/images/icon/icon-check.png';
 import Header from './Header';
 import HeaderMore from './HeaderMore';
 import WalletItem from './WalletItem';
