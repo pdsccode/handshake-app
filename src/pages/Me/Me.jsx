fefd113ae5112a45340a0e4aabcea058dbe2af05
0xtrunko <nhan@autonomous.nyc>
2018-06-01 11:11:15 +0700

    fix style

--- src/pages/Me/Me.jsx
+++ src/pages/Me/Me.jsx
@@ -41,7 +41,7 @@ class Me extends React.Component {
                   const FeedComponent = maps[handshake.type];
                   if (FeedComponent) {
                     return (
-                      <Col key={handshake.id} md={12} className="feed-wrapper">
+                      <Col key={handshake.id} className="feed-wrapper">
                         <FeedComponent {...handshake} {...this.props} onFeedClick={() => this.clickFeedDetail(handshake.id)} mode={'me'}/>
                       </Col>
                     );
