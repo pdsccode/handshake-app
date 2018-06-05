c1dfbdeb57fe4dbb6786b84252ad6e4fe73117da
0xbatutut <long.huynh+batutut@autonomous.nyc>
2018-06-05 18:14:17 +0700

    Clear log

--- webpack.config.js
+++ webpack.config.js
@@ -85,15 +85,15 @@ module.exports = function webpackConfig(env, argv) {
   const production = {
     optimization: {
       minimize: true,
-      // minimizer: [
-      //   new UglifyJsPlugin({
-      //     uglifyOptions: {
-      //       compress: {
-      //         drop_console: true,
-      //       },
-      //     },
-      //   }),
-      // ],
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+        }),
+      ],
       splitChunks: {
         chunks: 'all',
       },
