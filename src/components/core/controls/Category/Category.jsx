02b0be4baa5dea1bf89c4a61e90d7e7cf6ee21d6
Unknown <tam@autonomous.nyc>
2018-06-06 15:52:57 +0700

    Active bet on discover

--- src/components/core/controls/Category/Category.jsx
+++ src/components/core/controls/Category/Category.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // component
-// import Slider from '@/components/core/controls/Slider';
 import Image from '@/components/core/presentation/Image';
 // style
 import './Category.scss';
@@ -17,14 +16,6 @@ class Category extends React.PureComponent {
     // bind
     this.categoryClick.bind(this);
   }
-  // settings = {
-  //   dots: false,
-  //   infinite: false,
-  //   speed: 500,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 3,
-  //   arrows: false,
-  // };
 
   get categoriesHtml() {
     const { categories } = this.props;
@@ -41,6 +32,12 @@ class Category extends React.PureComponent {
     ))
   }
 
+  set idActive(id) {
+    this.setState({
+      idActive: id
+    });
+  }
+
   categoryClick(category) {
     this.setState({
       idActive: category.id
@@ -48,13 +45,19 @@ class Category extends React.PureComponent {
     this.props.hasOwnProperty('onItemClick') && this.props.onItemClick(category);
   }
 
+  componentDidMount() {
+    this.props.hasOwnProperty('onRef') && this.props.onRef(this);
+  }
+
+  componentWillUnmount() {
+    this.props.hasOwnProperty('onRef') && this.props.onRef(undefined);
+  }
+
   render() {
     const { className } = this.props;
     return (
       <div className={`category ${className || ''}`}>
-        {/* <Slider settings={this.settings}> */}
         {this.categoriesHtml}
-        {/* </Slider> */}
       </div>
     );
   }
@@ -64,6 +67,7 @@ Category.propType = {
   className: PropTypes.string,
   categories: PropTypes.array,
   onItemClick: PropTypes.func,
+  onRef: PropTypes.func,
 };
 
 export default Category;
