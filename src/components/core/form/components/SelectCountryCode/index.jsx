4a612ed5ac1758bc060a17db192835ec4f6b98ba
0xtrunko <nhan@autonomous.nyc>
2018-06-06 11:38:25 +0700

    improve country code selection

--- src/components/core/form/components/SelectCountryCode/index.jsx
+++ src/components/core/form/components/SelectCountryCode/index.jsx
@@ -1,20 +1,45 @@
 import React from "react";
 import COUNTRIES from "@/data/country-dial-codes.js";
 import "./styles.scss";
+import iconArrow from '@/assets/images/icon/Triangle.svg';
+
+// const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length )
 
 class SelectCountryCode extends React.Component {
   state = {
-    showSelect: false
+    showSelect: false,
+    strFilterCountry: ''
   };
 
+  // hideOnClickOutside = (element) => {
+  //   const outsideClickListener = event => {
+  //     if (!element.contains(event.target)) { // or use: event.target.closest(selector) === null
+  //       if (isVisible(element)) {
+  //         element.style.display = 'none'
+  //         removeClickListener()
+  //       }
+  //     }
+  //   }
+  //
+  //   const removeClickListener = () => {
+  //     document.removeEventListener('click', outsideClickListener)
+  //   }
+  //
+  //   document.addEventListener('click', outsideClickListener)
+  // }
+  // componentDidMount() {
+  //   this.hideOnClickOutside(document.getElementById("#select-cc-wrapper"))
+  // }
+
   toggleShowSelect = () => {
+    // console.log('onrlube', a.target)
     this.setState({
       showSelect: !this.state.showSelect
     });
     if (!this.state.showSelect) {
       setTimeout(() => {
         this.selectListCountryCode.focus();
-      }, 300);
+      }, 500);
     }
   };
 
@@ -24,26 +49,42 @@ class SelectCountryCode extends React.Component {
     this.toggleShowSelect();
   };
 
+  handleFilterChange = (e) => {
+    this.setState({ strFilterCountry: e.target.value });
+  }
+
   render() {
     const { countryCode, onChange } = this.props;
-    const { showSelect } = this.state;
+    const { showSelect, strFilterCountry } = this.state;
+    const filteredCountries = COUNTRIES.filter(c => c.name.toUpperCase().includes(strFilterCountry.toUpperCase()));
     return (
       <span>
-        <span role="button" onClick={this.toggleShowSelect}>
+        <span className="select-country-code" onClick={this.toggleShowSelect}>
           {countryCode}
+          <img className="ml-2" src={iconArrow} />
         </span>
         {
           showSelect && (
-            <div tabIndex="1" ref={(e) => { this.selectListCountryCode = e; }} className="select-list-country-code" onBlur={this.toggleShowSelect}>
-              {
-                COUNTRIES.map((item, index) => {
-                  const { dialCode, name, flag } = item;
-                  return (
-                    <div className="select-item-country-code" key={index}
-                         onClick={() => this.onItemClick(dialCode)}>{flag}{name} ({dialCode})</div>
-                  );
-                })
-              }
+            <div className="select-list-country-code-wrapper" tabIndex="1" ref={(e) => { this.selectListCountryCode = e; }}>
+              <div className="list">
+                <div>
+                  {
+                    filteredCountries && filteredCountries.length > 0 ? filteredCountries.map((item, index) => {
+                      const { dialCode, name, flag } = item;
+                      return (
+                        <div className="item" key={index}
+                             onClick={() => this.onItemClick(dialCode)}>{flag} {name} ({dialCode})</div>
+                      );
+                    }) : (
+                      <div>No data</div>
+                    )
+                  }
+                </div>
+              </div>
+              <div className="mt-2">
+                <input type="text" value={strFilterCountry} className="form-control-custom form-control-custom-ex input-no-border" placeholder="Filter" onChange={this.handleFilterChange}/>
+                <button onClick={this.toggleShowSelect} className="text-white btn btn-link float-right">Close</button>
+              </div>
             </div>
           )
         }
