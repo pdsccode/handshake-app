bc9d06a543efe949f0598b562cff02f31fc2eab4
Unknown <tam@autonomous.nyc>
2018-06-06 18:27:22 +0700

    update nodata.

--- src/components/core/presentation/NoData/NoData.jsx
+++ src/components/core/presentation/NoData/NoData.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // style
+import nodataNinjaSVG from '@/assets/images/ninja/nodata-ninja.svg';
 import './NoData.scss';
 
 class NoData extends React.PureComponent {
@@ -8,7 +9,8 @@ class NoData extends React.PureComponent {
     const { className, message, ...props } = this.props;
     return (
       <div className={`no-data ${className || ''}`} {...props}>
-          <p className="text">{ message || 'NO DATA AVAILABLE' }</p>
+        <img className="ninja img-fluid" src={nodataNinjaSVG} alt="nodata ninja" />
+        <p className="text">{ message || 'NO DATA AVAILABLE' }</p>
       </div>
     );
   }
