a83c595f68addd797412ea4cac1840bde89056d8
Unknown <tam@autonomous.nyc>
2018-06-07 16:16:37 +0700

    Update user profile

---
+++ src/models/Auth.js
@@ -0,0 +1,32 @@
+/**
+ * @class Auth
+ */
+class Auth {
+  /**
+   *
+   *
+   * @static
+   * @param {*} [rawData={}]
+   * @memberof Auth
+   */
+  static profile(rawData = {}) {
+    return {
+      avatar: rawData.avatar || '',
+      cardId: rawData.card_id || -1,
+      cardVerified: rawData.card_verified || '',
+      dateCreated: rawData.date_created || '',
+      dateModified: rawData.date_modified || '',
+      email: rawData.email || '',
+      fcmToken: rawData.fcm_token || '',
+      id: rawData.id || -1,
+      name: rawData.name || '',
+      phone: rawData.phone || '',
+      rewardWalletAddresses: rawData.reward_wallet_addresses || '',
+      status: rawData.status || '',
+      username: rawData.username || '',
+      address: rawData.address || '',
+    }
+  }
+}
+
+export default Auth;
