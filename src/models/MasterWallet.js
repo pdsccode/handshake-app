4986496cfd30b9efa9a6019dab35c4e50ce35767
0xngoubou <Phuong_Mac@Phuong-MacBook-Pro.local>
2018-06-12 13:27:17 +0700

    update bg image for wallet + css walelt selected default

--- src/models/MasterWallet.js
+++ src/models/MasterWallet.js
@@ -82,18 +82,22 @@ export class MasterWallet {
 
     // for create new wallets:
     static createNewsallets(listCoinTemp, mnemonic) {
-      console.log('mnemonic', mnemonic);
+      let isImport = false;
       if (mnemonic == '') {
         mnemonic = bip39.generateMnemonic(); // generates string
       } else if (!bip39.validateMnemonic(mnemonic)) {
         console.log('validateMnemonic mnemonic', false);
         return false;
       }
+      else {
+        isImport = true;
+      }
       const masterWallet = MasterWallet.getMasterWallet();
       listCoinTemp.forEach((wallet) => {
         if (wallet.default) {
           wallet.default = false;
           wallet.mnemonic = mnemonic;
+          wallet.protected = isImport;
           // create address, private-key ...
           wallet.createAddressPrivatekey();
           masterWallet.push(wallet);
