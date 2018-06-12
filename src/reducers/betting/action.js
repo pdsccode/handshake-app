6ac099bdf4ae3a906244d7e1234d31b3efb6f719
0xbatutut <long.huynh+batutut@autonomous.nyc>
2018-06-12 14:15:58 +0700

    misc

--- src/reducers/betting/action.js
+++ src/reducers/betting/action.js
@@ -2,11 +2,10 @@
 import { createAPI } from '@/reducers/action';
 
 export const BETTING_ACTIONS = {
-    LOAD_MATCHES: 'LOAD_MATCHES',
-    LOAD_HANDSHAKES: 'LOAD_HANDSHAKES',
-}
+  LOAD_MATCHES: 'LOAD_MATCHES',
+  LOAD_HANDSHAKES: 'LOAD_HANDSHAKES',
+};
 
 export const loadMatches = createAPI(BETTING_ACTIONS.LOAD_MATCHES);
 export const loadHandshakes = createAPI(BETTING_ACTIONS.LOAD_HANDSHAKES);
-
 
