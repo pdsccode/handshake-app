e29fc47c8f854ec3f760fb573cb1c6b3b2fc6112
0xngoubou <Phuong_Mac@Phuong-MacBook-Pro.local>
2018-06-12 12:03:57 +0700

    fix update balance store

--- src/models/MasterWallet.js
+++ src/models/MasterWallet.js
@@ -111,7 +111,7 @@ export class MasterWallet {
     static UpdateBalanceItem(item){
       let wallets = MasterWallet.getMasterWallet();
       wallets.forEach((wallet) => {
-        if (wallet.address == item.network && wallet.network == item.network) {
+        if (wallet.address == item.address && wallet.network == item.network) {
           wallet.balance = item.balance;
         }
       });
