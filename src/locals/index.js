a71d5345332c6e8a6d90b5eeddb17529219cdfc7
0xbatutut <long.huynh+batutut@autonomous.nyc>
2018-06-13 17:02:57 +0700

    Update languages

--- src/locals/index.js
+++ src/locals/index.js
@@ -3,9 +3,18 @@ import fr from './fr';
 import vi from './vi';
 import zh from './zh';
 import de from './de';
-import ja from './ja'
+import ja from './ja';
 import ko from './ko';
 import ru from './ru';
 import es from './es';
 
-export default { en, fr, vi, zh, de, ja, ko, ru, es };
+const languages = {
+  en, fr, vi, zh, de, ja, ko, ru, es,
+};
+const keys = Object.keys(languages);
+
+keys.forEach((key) => {
+  languages[key] = { ...en, ...languages[key] };
+});
+
+export default languages;
