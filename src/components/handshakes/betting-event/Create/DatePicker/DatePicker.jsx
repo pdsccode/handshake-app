4dcafea34b18ec6cc26df74db8f01b9d5517bd00
Mitla <40157160+Poukai@users.noreply.github.com>
2018-06-25 12:11:32 +0700

    Feature/betting event (#260)
    * events input fields and date picker init
    
    * input fields and calendar mapped to state of the class
    
    * calendar state fix
    
    * create betting event form fields and post outcome call
    
    * add new outcomes to existing events
    
    * datepicker updated
    
    * Fix bug not display amount and winValue
    
    * Revert "Fix bug not display amount and winValue"
    
    This reverts commit 87305a6177c158b4005cc81c0e216f07dc1432aa.
    
    * moment js updated and date input fix
    
    * add match in progress
    
    * password field updated , set token , fetch matches
    
    * datepicker result to iso, create event button
    
    * headers in api.js moved below parsedInput in merge
    
    * changed date o/p to unix
    
    * showed error message in modal
    
    * submit and cancel button placement
    
    * cancel button
    
    * only password input no md5
    
    * Remove home score and away score
    
    * closingTime  < reportingTime < DisputeTime
    
    * reset state after cancel click
    
    * redirect and error message , removed creatorFee from outcomes
    
    * predictionHS.js file updated
    
    * url to redirect after success
    
    * error message in form submmit
    
    * view allowed month fix
    
    * close calendar on select

--- src/components/handshakes/betting-event/Create/DatePicker/DatePicker.jsx
+++ src/components/handshakes/betting-event/Create/DatePicker/DatePicker.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Datetime from 'react-datetime';
 import 'react-datetime/css/react-datetime.css';
 
-import moment from 'moment';
-
 // style
 import './DatePicker.scss';
+import DetailBettingEvent from '@/components/handshakes/betting-event/Detail/Detail';
+
+const moment = require('moment');
+
+const yesterday = Datetime.moment().subtract(1, 'day');
 
 class DatePicker extends React.PureComponent {
   constructor(props) {
@@ -15,24 +18,95 @@ class DatePicker extends React.PureComponent {
     // };
     this.onChangeDate = this.onChangeDate.bind(this);
   }
+
   selectedDate = moment();
 
   get value() {
     return this.selectedDate;
   }
 
+  isDate = date => (!!((new Date(date) !== 'Invalid Date' && !isNaN(new Date(date)))))
+
   onChangeDate(date) {
     // this.setState({ selectedDate: date });
     this.selectedDate = date;
     console.log('Date:', this.selectedDate);
     const { onChange } = this.props;
-    const unixDate = date.unix();
-    onChange && onChange(unixDate);
+    console.log('test');
+    const unixDate = this.isDate(date) ? date.unix() : date;
+    if (this.isDate(date)) {
+      onChange && onChange(unixDate);
+    } else {}
   }
+  // separate date validitiy based on type of form = reporting will have closing time and type will be reporting similarly dispute will have reporting time
+  valid = (current) => {
+    switch (this.props.name) {
+      case 'closingTime':
+        return current.isAfter(yesterday);
+
+      case 'reportingTime':
+        return current.isAfter(moment.unix(this.props.startDate));
+
+      case 'disputeTime':
+        return current.isAfter(moment.unix(this.props.startDate));
+
+      default:
+        return current.isAfter(yesterday);
+    }
+  };
 
   render() {
     const { className, onChange, ...props } = this.props;
-    return (<Datetime onChange={this.onChangeDate} {...props} inputProps={{ placeholder: this.props.placeholder, className: this.props.className }} />);
+    switch (this.props.name) {
+      case 'closingTime':
+        return (<Datetime
+          onChange={this.onChangeDate}
+          {...props}
+          isValidDate={this.valid}
+          style
+          inputProps={{
+      placeholder: this.props.placeholder, className: this.props.className, required: this.props.required, readOnly: true, disabled: this.props.disabled,
+      }}
+        />);
+
+      case 'reportingTime':
+        return (<Datetime
+          onChange={this.onChangeDate}
+          {...props}
+          isValidDate={this.valid}
+          style
+          closeOnSelect
+          viewDate={`${new Date(this.props.startDate * 1000)}`}
+          inputProps={{
+      placeholder: this.props.placeholder, className: this.props.className, required: this.props.required, readOnly: true, disabled: this.props.disabled,
+      }}
+        />);
+
+      case 'disputeTime':
+        return (<Datetime
+          onChange={this.onChangeDate}
+          {...props}
+          isValidDate={this.valid}
+          style
+          closeOnSelect
+          viewDate={`${new Date(this.props.startDate * 1000)}`}
+          inputProps={{
+      placeholder: this.props.placeholder, className: this.props.className, required: this.props.required, readOnly: true, disabled: this.props.disabled,
+      }}
+        />);
+
+      default:
+        return (<Datetime
+          onChange={this.onChangeDate}
+          {...props}
+          closeOnSelect
+          isValidDate={this.valid}
+          style
+          inputProps={{
+      placeholder: this.props.placeholder, className: this.props.className, required: this.props.required, readOnly: true, disabled: this.props.disabled,
+      }}
+        />);
+    }
   }
 }
 
