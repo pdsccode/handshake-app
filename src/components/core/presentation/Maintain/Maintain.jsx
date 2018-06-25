de3e4fe9945ea2975822b7b541abd05e2aed6560
Batutut <long.huynh+batutut@autonomous.nyc>
2018-06-25 14:22:49 +0700

    Update UI maintain (#261)

--- src/components/core/presentation/Maintain/Maintain.jsx
+++ src/components/core/presentation/Maintain/Maintain.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 // style
-import nodataNinjaSVG from '@/assets/images/ninja/nodata-ninja.svg';
+import blockcountrySVG from '@/assets/images/pages/block-country/block-country.svg';
+import SpiderImg1 from '@/assets/images/maintain/spider.png';
+import SpiderImg2 from '@/assets/images/maintain/spider2.png';
 
 import './Maintain.scss';
 
 class Maintain extends React.PureComponent {
-
   render() {
     return (
-      <div className="blockCountry">
-        <img className="img-fluid img" src={nodataNinjaSVG} alt="noconnection ninja" />
-        <p>We are maintaining</p>
+      <div className="maintain-tab">
+        <img src={SpiderImg1} alt="" className="spider spider-1" />
+        <img src={SpiderImg2} alt="" className="spider spider-2" />
+        <img className="img-fluid img img-center" src={blockcountrySVG} alt="noconnection ninja" />
+        <p className="title">OUT TO LUNCH.</p>
+        <p className="title">BACK SOON.</p>
+        <p>Damn bugs got all over the picnic table.</p>
+        <p>See you 29/6</p>
       </div>
     );
   }
