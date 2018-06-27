b80c1132b5b6adac41a04484b235175bd032c281
Hakawai <vtkimsa@gmail.com>
2018-06-27 11:39:17 +0700

    Feature/betting dev (#272)
    * events input fields and date picker init
    
    * input fields and calendar mapped to state of the class
    
    * calendar state fix
    
    * create betting event form fields and post outcome call
    
    * add new outcomes to existing events
    
    * datepicker updated
    
    * Fix bug not display amount and winValue
    
    * Change Math.floor to Math.round
    
    * convert to get round odds submit to server
    
    * don't let user make an action if his wallet isn't enough
    
    * Fix bug show wrong message
    
    * Revert Math.round to Math.floor
    
    * fix bug missing import MESSAGE
    
    * Fix bug update status after click Cancel/Withdraw/Refund
    
    * update UI trang me
    
    * Add rollback status, change Math.round to Math.floor
    
    * Close free bet
    
    * update css betting
    
    * Show alert message if rollback
    change address and PredictionHandshake.json
    
    * Change text at Feed
    Close Free Bet
    
    * Set color for withdraw/cancel button
    
    * Show popup rollback
    Open free bet again
    
    * round 1 decimal
    
    * Change to new address and PredictionHandshake.json file for staging
    
    * open freebet
    
    * Add free_bet variable
    Add address and PredictionHandshake.json for Live
    
    * Add Collect Free Bet
    
    * Add succesful message for withdraw
    
    * Fix bug withdraw
    
    * Fix withdraw for real money
    
    * Fix bug difference address
    
    * Fix bug show status wrong
    
    * log
    
    * hide market fee
    
    * fix neutron
    
    * Fix bug update UI when withdraw
    Get estimate gas from limit gas
    change limit gas to 350000
    
    * fix neutron
    
    * Add message handle rollback blockchain
    
    * add try catch when call contract
    refactor when find shake item
    
    * Add private/public
    
    * Fix bug try catch when adding contract
    
    * Add API Uninit Free
    
    * show message failed from server
    
    * change API uninit free bet
    implement filter outcome
    
    * Fix bug Public/Private
    
    * Add utils file
    Move some functions in BetHandshakeHandler to utils file

--- src/components/handshakes/betting/Create/Create.jsx
+++ src/components/handshakes/betting/Create/Create.jsx
@@ -22,6 +22,9 @@ import { MasterWallet } from '@/models/MasterWallet';
 import Dropdown from '@/components/core/controls/Dropdown';
 import Toggle from '@/components/handshakes/betting/Feed/Toggle';
 import { showAlert } from '@/reducers/app/action';
+import {isRightNetwork, isExpiredDate, getChainIdDefaultWallet, 
+  getBalance, getEstimateGas, isExistMatchBet} from '@/components/handshakes/betting/utils.js';
+
 // self
 import { InputField } from '../form/customField';
 import ErrorBox from './ErrorBox';
@@ -146,9 +149,11 @@ class BettingCreate extends React.Component {
       const foundMatch = this.foundMatch;
       if (foundMatch) {
         const { outcomes } = foundMatch;
-        if (outcomes) {
+        let filterOutcome = outcomes.filter(item => item.public == 1);
+
+        if (filterOutcome) {
           // return outcomes.map((item) => ({ id: item.id, value: item.name, hid: item.hid}));
-          return outcomes.map(item => ({
+          return filterOutcome.map(item => ({
             id: item.id, value: `Outcome: ${item.name}`, hid: item.hid, marketOdds: item.market_odds, marketAmount: item.market_amount,
           }));
         }
@@ -222,9 +227,9 @@ class BettingCreate extends React.Component {
     // const {toAddress, isPublic, industryId} = this.props;
 
     // const fromAddress = "0x54CD16578564b9952d645E92b9fa254f1feffee9";
-    let balance = await betHandshakeHandler.getBalance();
+    let balance = await getBalance();
     balance = parseFloat(balance);
-    const estimatedGas = await betHandshakeHandler.getEstimateGas();
+    const estimatedGas = await getEstimateGas();
     // const estimatedGas = 0.00001;
     console.log('Estimate Gas:', estimatedGas);
     const eventBet = parseFloat(values.event_bet);
@@ -235,19 +240,18 @@ class BettingCreate extends React.Component {
     const total = eventBet + parseFloat(estimatedGas);
     console.log('Event Bet, Odds, Estimate, Total:', eventBet, odds, estimatedGas, total);
 
-    const fromAddress = betHandshakeHandler.getAddress();
+    const fromAddress = getAddress();
     console.log('Match, Outcome:', selectedMatch, selectedOutcome);
 
     let message = null;
     const date = selectedMatch.date;
     console.log('Date:', date);
-    if (!betHandshakeHandler.isRightNetwork()) {
+    if (!isRightNetwork()) {
       message = MESSAGE.RIGHT_NETWORK;
     }
 
-    if(selectedMatch && selectedOutcome){
-      if (betHandshakeHandler.isExpiredDate(date)){
-
+    if (selectedMatch && selectedOutcome) {
+      if (isExpiredDate(date)) {
         message = MESSAGE.MATCH_OVER;
       } else if (eventBet > 0) {
         if (total <= balance) {
@@ -559,7 +563,7 @@ display: 'flex', flexDirection: 'column', flex: 1, marginBottom: 10,
       currency: 'ETH',
       side: parseInt(side),
       from_address: fromAddress,
-      chain_id: betHandshakeHandler.getChainIdDefaultWallet(),
+      chain_id: getChainIdDefaultWallet(),
     };
     console.log('Go to Params:', params);
     const hid = selectedOutcome.hid;
@@ -584,7 +588,7 @@ display: 'flex', flexDirection: 'column', flex: 1, marginBottom: 10,
     const hid = selectedOutcome.hid;
 
     if (status && data) {
-      const isExist = betHandshakeHandler.isExistMatchBet(data);
+      const isExist = isExistMatchBet(data);
       let message = MESSAGE.CREATE_BET_NOT_MATCH;
       if (isExist) {
         message = MESSAGE.CREATE_BET_MATCHED;
