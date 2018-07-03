728bed2a06e6aa625b873dd1233012063c04f09b
0xtrunko <nhan@autonomous.nyc>
2018-07-03 15:06:35 +0700

    fix default

--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -150,7 +150,7 @@ class DiscoverPage extends React.Component {
     if (window.location.pathname.indexOf(URL.HANDSHAKE_CASH) >= 0) {
       return HANDSHAKE_ID.EXCHANGE;
     }
-    let seletedId = HANDSHAKE_ID.BETTING;
+    let seletedId = HANDSHAKE_ID.EXCHANGE;
     let { id } = Helper.getQueryStrings(window.location.search);
     id = parseInt(id, 10);
     if (id && Object.values(HANDSHAKE_ID).indexOf(id) !== -1) {
