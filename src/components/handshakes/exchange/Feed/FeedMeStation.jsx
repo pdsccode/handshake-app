789a7eb5662abcbba0496d18133a036ca71d3fdd
0xmegalodon <dung.thanh@autonomous.nyc>
2018-07-11 14:34:35 +0700

    Show data of dashboard

--- src/components/handshakes/exchange/Feed/FeedMeStation.jsx
+++ src/components/handshakes/exchange/Feed/FeedMeStation.jsx
@@ -1,18 +1,88 @@
-import React from 'react'
-import './FeedMe.scss'
-import './FeedMeStation.scss'
-import { URL } from '@/constants'
-import { daysBetween, formatAmountCurrency, formatMoneyByLocale } from '@/services/offer-util'
+import React from 'react';
+import './FeedMe.scss';
+import './FeedMeStation.scss';
+import { FIAT_CURRENCY, URL } from '@/constants';
+import { daysBetween, formatAmountCurrency, formatMoneyByLocale } from '@/services/offer-util';
 import iconSpinner from '@/assets/images/icon/icons8-spinner.gif';
 import iconAvatar from '@/assets/images/icon/avatar.svg';
 import StarsRating from '@/components/core/presentation/StarsRating';
 import CoinCards from '@/components/handshakes/exchange/components/CoinCards';
-import {FormattedMessage} from "react-intl";
+import { FormattedMessage, injectIntl } from 'react-intl';
+import Modal from '@/components/core/controls/Modal';
+import Button from '@/components/core/controls/Button';
+import { connect } from 'react-redux';
+import { showAlert } from '@/reducers/app/action';
+
+window.Clipboard = (function (window, document, navigator) {
+  let textArea,
+    copy; function isOS() { return navigator.userAgent.match(/ipad|iphone/i); } function createTextArea(text) { textArea = document.createElement('textArea'); textArea.value = text; document.body.appendChild(textArea); } function selectText() {
+    let range,
+      selection; if (isOS()) { range = document.createRange(); range.selectNodeContents(textArea); selection = window.getSelection(); selection.removeAllRanges(); selection.addRange(range); textArea.setSelectionRange(0, 999999); } else { textArea.select(); }
+  } function copyToClipboard() { document.execCommand('copy'); document.body.removeChild(textArea); } copy = function (text) { createTextArea(text); selectText(); copyToClipboard(); }; return { copy };
+}(window, document, navigator));
 
 class FeedMeStation extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-  render () {
-    // console.log('thisss', this.props);
+    this.state = {
+      walletsData: false,
+      inputRestoreWalletValue: '',
+    };
+  }
+
+  handleFocus = (e) => {
+    e.currentTarget.select();
+  }
+
+  showToast(mst) {
+    this.showAlert(mst, 'primary', 2000);
+  }
+
+  updateRestoreWalletValue = (evt) => {
+    this.setState({
+      inputRestoreWalletValue: evt.target.value,
+    });
+  }
+
+  render() {
+    const { messages } = this.props.intl;
+    const { dashboardInfo, offerStores } = this.props;
+
+    let transactionSuccessful = 0;
+    let transactionFailed = 0;
+    let transactionPending = 0;
+    const transactionRevenue = [];
+
+    if (dashboardInfo) {
+      dashboardInfo.forEach((item) => {
+        // console.log('item', item);
+        transactionSuccessful += item.success;
+        transactionFailed += item.failed;
+        transactionPending += item.pending;
+
+        Object.values(FIAT_CURRENCY).forEach((currency) => {
+          const buy = item.buyFiatAmounts[currency];
+          const sell = item.sellFiatAmounts[currency];
+
+          if (buy || sell) {
+            transactionRevenue.push(
+            <div className="d-table w-100 station-info">
+              <div className="d-table-cell align-middle label"><FormattedMessage id="ex.shop.dashboard.label.revenue" /> {item.currency}</div>
+              <div className="d-table-cell align-middle text-right info">{formatMoneyByLocale(buy?.amount || 0, currency)}/{formatMoneyByLocale(sell?.amount || 0, currency)} {currency}</div>
+            </div>);
+          }
+        });
+      });
+    }
+
+    const transactionTotal = dashboardInfo && dashboardInfo.map(item => (
+      <div className="d-table w-100 station-info">
+        <div className="d-table-cell align-middle label"><FormattedMessage id="ex.shop.dashboard.label.transaction.total" /> {item.currency}</div>
+        <div className="d-table-cell align-middle text-right info">{formatAmountCurrency(item.buyAmount)}/{formatAmountCurrency(item.sellAmount)}</div>
+      </div>));
+
+    console.log('thisss', this.props);
     return (
       <div>
         <div className="feed-me-station">
@@ -27,7 +97,7 @@ class FeedMeStation extends React.PureComponent {
             </div>
           </div>
 
-          <CoinCards coins={[]} currency={'ETH'} />
+          <CoinCards coins={[]} currency="ETH" />
 
           <div className="d-table w-100 mt-2">
             <div className="d-table-cell align-middle" style={{ width: '42px' }}>
@@ -44,25 +114,63 @@ class FeedMeStation extends React.PureComponent {
 
           <div className="mt-3 d-table w-100 station-info">
             <div className="d-table-cell align-middle label"><FormattedMessage id="ex.shop.dashboard.label.transaction.successfull.failed" /></div>
-            <div className="d-table-cell align-middle text-right info">60/30</div>
+            <div className="d-table-cell align-middle text-right info">{`${transactionSuccessful}/${transactionFailed}`}</div>
           </div>
           <div className="d-table w-100 station-info">
             <div className="d-table-cell align-middle label"><FormattedMessage id="ex.shop.dashboard.label.transaction.pending" /></div>
-            <div className="d-table-cell align-middle text-right info">30</div>
+            <div className="d-table-cell align-middle text-right info">{transactionPending}</div>
           </div>
-          <div className="d-table w-100 station-info">
-            <div className="d-table-cell align-middle label"><FormattedMessage id="ex.shop.dashboard.label.revenue" /></div>
-            <div className="d-table-cell align-middle text-right info">30</div>
-          </div>
-          <div className="d-table w-100 station-info">
-            <div className="d-table-cell align-middle label"><FormattedMessage id="ex.shop.dashboard.label.transaction.total" /></div>
-            <div className="d-table-cell align-middle text-right info">30</div>
-          </div>
+
+          {
+            transactionRevenue && transactionRevenue
+          }
+          {
+            transactionTotal && transactionTotal
+          }
+
         </div>
         <button className="btn btn-primary btn-block">Backup</button>
+        {/* Modal for Backup wallets : */}
+        <Modal title={messages.wallet.action.backup.header} onRef={modal => this.modalBackupRef = modal}>
+          <div className="bodyTitle">{messages.wallet.action.backup.description}</div>
+          <div className="bodyBackup">
+            <textarea
+              readOnly
+              onClick={this.handleChange}
+              onFocus={this.handleFocus}
+              value={this.state.walletsData ? JSON.stringify(this.state.walletsData) : ''}
+            />
+            <Button className="button" cssType="danger" onClick={() => { Clipboard.copy(JSON.stringify(this.state.walletsData)); this.modalBackupRef.close(); this.showToast(messages.wallet.action.backup.success.copied); }} >{messages.wallet.action.backup.button.copy}</Button>
+          </div>
+        </Modal>
+
+        {/* Modal for Restore wallets : */}
+        <Modal title={messages.wallet.action.restore.header} onRef={modal => this.modalRestoreRef = modal}>
+          <div className="bodyTitle">{messages.wallet.action.restore.description}</div>
+          <div className="bodyBackup">
+            <textarea
+              required
+              value={this.state.inputRestoreWalletValue}
+              className={this.state.erroValueBackup ? 'error' : ''}
+              onChange={evt => this.updateRestoreWalletValue(evt)}
+            />
+            <Button isLoading={this.state.isRestoreLoading} className="button" cssType="danger" onClick={() => { this.restoreWallets(); }} >
+              {messages.wallet.action.restore.button.restore}
+            </Button>
+          </div>
+        </Modal>
       </div>
-    )
+    );
   }
 }
 
-export default FeedMeStation
+const mapState = state => ({
+  offerStores: state.exchange.offerStores,
+  dashboardInfo: state.exchange.dashboardInfo,
+});
+
+const mapDispatch = ({
+  showAlert,
+});
+
+export default injectIntl(connect(mapState, mapDispatch)(FeedMeStation));
