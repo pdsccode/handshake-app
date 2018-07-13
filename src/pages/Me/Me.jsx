f9368fb0732694b08a341587c096338d24669d53
0xmegalodon <dung.thanh@autonomous.nyc>
2018-07-13 10:31:38 +0700

    If betting not have go to Cash tab

--- src/pages/Me/Me.jsx
+++ src/pages/Me/Me.jsx
@@ -110,6 +110,7 @@ class Me extends React.Component {
       cashTab: CASH_TAB.TRANSACTION,
       handshakeIdActive: handshakeDefault,
       firstTime: true,
+      me: this.props.me,
     };
   }
 
@@ -172,7 +173,8 @@ class Me extends React.Component {
       }
     }
 
-    if (nextProps.me.list.length === 0 && prevState.handshakeIdActive === HANDSHAKE_ID.BETTING && prevState.firstTime) {
+    if (nextProps.me.list.length === 0 && nextProps.me.list.updateAt !== prevState.me.list.updateAt
+      && prevState.handshakeIdActive === HANDSHAKE_ID.BETTING && prevState.firstTime) {
       rfChange(nameFormFilterFeeds, 'feedType', HANDSHAKE_ID.EXCHANGE);
       return { handshakeIdActive: HANDSHAKE_ID.EXCHANGE, firstTime: false };
     }
