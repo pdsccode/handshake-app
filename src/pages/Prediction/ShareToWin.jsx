5c4e01e6c9a14b9e932fde554e43c991873e54a9
Nghia Mai <nghia@autonomous.nyc>
2018-07-19 17:46:21 +0700

    update router

---
+++ src/pages/Prediction/ShareToWin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ShareToWin() {
+  return (
+    <div className="ShareToWin">
+      <div className="ShareToWinTitle">
+        SHARE TO <span>WIN 10 ETH</span>
+      </div>
+    </div>
+  );
+}
+
+export default ShareToWin;
