98d4867280a9e3b483d47e0952482838550f2e2d
Moehau <binh@autonomous.nyc>
2018-07-30 18:10:35 +0700

    fix ui

--- src/services/agent.js
+++ src/services/agent.js
@@ -4,7 +4,7 @@ import superagent from 'superagent'
 const req = superagentPromise(superagent, global.Promise);
 
 //const API_ROOT = 'http://35.198.228.87';
-const API_ROOT = 'https://dad.ninja.org/api';
+const API_ROOT = 'https://dad.ninja.org';
 //const API_ROOT = 'http://localhost:8000';
 
 export default {API_ROOT, req};
