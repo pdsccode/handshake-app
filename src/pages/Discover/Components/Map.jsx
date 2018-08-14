572d0e402b6e5a2727f66bd4b1fe76133934925e
0xtrunko <nhan@autonomous.nyc>
2018-08-14 15:35:06 +0700

    add current location indicator

--- src/pages/Discover/Components/Map.jsx
+++ src/pages/Discover/Components/Map.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
+import {
+  GoogleMap,
+  Marker,
+  withGoogleMap,
+  withScriptjs,
+} from 'react-google-maps';
 
 import StationMarker from './StationMarker';
 import './Map.scss';
@@ -7,37 +12,21 @@ import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 
 import iconCurrentLocation from '@/assets/images/icon/current-location.svg';
+import currentLocationIndicator from '@/assets/images/icon/current-location-indicator.png';
 import OfferShop from '@/models/OfferShop';
 
-
 class Map extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const { lat, lng } = this.props;
-
-    this.state = {
-      center: { lat, lng },
-      zoomLevel: 6,
-    };
-  }
-  componentDidMount() {
-    const { ipInfo } = this.props;
-
-    this.setAddressFromLatLng(ipInfo?.latitude, ipInfo?.longitude); // fallback
-  }
-
   setAddressFromLatLng = (lat, lng) => {
-    this.setState({ center: { lat, lng } }, () => {
-    });
-  }
-
+    this.setState({ center: { lat, lng } }, () => {});
+  };
   handleGoToCurrentLocation = () => {
     const { ipInfo } = this.props;
-    this.setState({ center: { lat: ipInfo?.latitude, lng: ipInfo?.longitude }, zoomLevel: 15 });
-  }
-
-  isEmptyBalance = (item) => {
+    this.setState({
+      center: { lat: ipInfo?.latitude, lng: ipInfo?.longitude },
+      zoomLevel: 15,
+    });
+  };
+  isEmptyBalance = item => {
     if (!item) {
       return;
     }
@@ -48,45 +37,78 @@ class Map extends React.Component {
       return sellAmount <= 0;
     }
     return buyAmount <= 0;
+  };
+
+  constructor(props) {
+    super(props);
+
+    const { lat, lng } = this.props;
+
+    this.state = {
+      center: { lat, lng },
+      zoomLevel: 15,
+    };
+  }
+
+  componentDidMount() {
+    const { ipInfo } = this.props;
+
+    this.setAddressFromLatLng(ipInfo?.latitude, ipInfo?.longitude); // fallback
   }
 
   render() {
     const {
-      isMarkerShown, onMarkerClick, stations, actionActive, currencyActive, onFeedClick, modalRef, setLoading, history,
+      isMarkerShown,
+      onMarkerClick,
+      stations,
+      actionActive,
+      currencyActive,
+      onFeedClick,
+      modalRef,
+      setLoading,
+      history,
     } = this.props;
     const { center, zoomLevel } = this.state;
 
     return (
-      <GoogleMap
-        zoom={zoomLevel}
-        center={center}
-      >
-        {
-          stations && stations.map((station) => {
+      <GoogleMap zoom={zoomLevel} center={center}>
+        {stations &&
+          stations.map(station => {
             const { id, ...rest } = station;
             const offer = OfferShop.offerShop(JSON.parse(station.extraData));
-            const allowRender = offer.itemFlags[currencyActive] && !this.isEmptyBalance(offer.items[currencyActive]);
+            const allowRender =
+              offer.itemFlags[currencyActive] &&
+              !this.isEmptyBalance(offer.items[currencyActive]);
 
             if (!allowRender) {
               return null;
             }
-              return (
-                <StationMarker
-                  key={id}
-                  {...rest}
-                  actionActive={actionActive}
-                  currencyActive={currencyActive}
-                  onFeedClick={extraData => onFeedClick(station, extraData)}
-                  offer={offer}
-                  modalRef={modalRef}
-                  setLoading={setLoading}
-                />
-              );
-          })
-        }
-        <button className="btn-current-location" onClick={this.handleGoToCurrentLocation}>
+            return (
+              <StationMarker
+                key={id}
+                {...rest}
+                actionActive={actionActive}
+                currencyActive={currencyActive}
+                onFeedClick={extraData => onFeedClick(station, extraData)}
+                offer={offer}
+                modalRef={modalRef}
+                setLoading={setLoading}
+              />
+            );
+          })}
+        <button
+          className="btn-current-location"
+          onClick={this.handleGoToCurrentLocation}
+        >
           <img src={iconCurrentLocation} />
         </button>
+        <Marker
+          defaultIcon={{
+            url: currentLocationIndicator,
+            scaledSize: { width: 30, height: 30 },
+          }}
+          position={center}
+        />
       </GoogleMap>
     );
   }
@@ -100,5 +122,6 @@ const mapDispatch = dispatch => ({
   // rfChange: bindActionCreators(change, dispatch),
 });
 
-export default injectIntl(connect(mapState, mapDispatch)(withScriptjs(withGoogleMap(Map))));
-
+export default injectIntl(
+  connect(mapState, mapDispatch)(withScriptjs(withGoogleMap(Map))),
+);
