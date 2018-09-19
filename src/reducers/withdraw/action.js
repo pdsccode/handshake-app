181db8b335d6ecabaad5403026f161934b7fe7c9
Keresh <vuong@ninja.org>
2018-09-19 11:20:13 +0700

    Remove duplicate files

--- src/reducers/withdraw/action.js
+++
@@ -1,9 +0,0 @@
-import { createAPI } from '@/reducers/action';
-
-export const ACTIONS = {
-  LOAD_WITHDRAW: 'LOAD_WITHDRAW',
-  COMPLETE_WITHDRAW: 'COMPLETE_WITHDRAW',
-};
-
-export const loadWithdrawList = createAPI(ACTIONS.LOAD_WITHDRAW);
-export const completeWithdraw = createAPI(ACTIONS.COMPLETE_WITHDRAW);
