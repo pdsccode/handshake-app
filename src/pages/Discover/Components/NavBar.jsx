d8994e79d13b4c0b2f918383a252efc0e78b0040
0xmegalodon <dung.thanh@autonomous.nyc>
2018-09-27 14:55:32 +0700

    Update layout map: update list coin

--- src/pages/Discover/Components/NavBar.jsx
+++ src/pages/Discover/Components/NavBar.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Field } from 'redux-form';
-import createForm from '@/components/core/form/createForm';
-import { fieldDropdown, fieldRadioButton } from '@/components/core/form/customField';
 import { injectIntl } from 'react-intl';
 import './NavBar.scss';
 import {
@@ -12,7 +9,12 @@ import {
   EXCHANGE_ACTION,
   EXCHANGE_ACTION_COLORS,
   EXCHANGE_ACTION_NAME,
+  FIAT_CURRENCY,
 } from '@/constants';
+import iconBitcoin from '@/assets/images/icon/coin/btc.svg';
+import iconEthereum from '@/assets/images/icon/coin/eth.svg';
+import iconBitcoinCash from '@/assets/images/icon/coin/bch.svg';
+import { formatMoneyByLocale, getOfferPrice } from '@/services/offer-util';
 
 const listAction = Object.values(EXCHANGE_ACTION).map((item) => {
   return { value: item, text: EXCHANGE_ACTION_NAME[item], bgColorActive: EXCHANGE_ACTION_COLORS[item].color };
@@ -22,53 +24,46 @@ const listCoin = Object.values(CRYPTO_CURRENCY).map((item) => {
   return { id: item, text: <span><img src={CRYPTO_CURRENCY_COLORS[item].icon} width={25} /> {CRYPTO_CURRENCY_NAME[item]}</span> };
 });
 
-import iconMyATM from './icons8-location_off.svg';
-
-import iconBitcoin from '@/assets/images/icon/coin/btc.svg';
-import iconEthereum from '@/assets/images/icon/coin/eth.svg';
-import iconBitcoinCash from '@/assets/images/icon/coin/bch.svg';
-
-const iconMap = {
-  ETH: iconEthereum,
-  BTC: iconBitcoin,
+export const CRYPTO_ICONS = {
+  [CRYPTO_CURRENCY.ETH]: iconEthereum,
+  [CRYPTO_CURRENCY.BTC]: iconBitcoin,
   BCH: iconBitcoinCash,
-}
+};
+
+const CRYPTO_CURRENCY_CREDIT_CARD = {
+  ...CRYPTO_CURRENCY, BCH: 'BCH',
+};
+
+const listCurrency = Object.values(CRYPTO_CURRENCY_CREDIT_CARD).map((item) => {
+  return { id: item, text: <img src={CRYPTO_ICONS[item]} width={24} /> };
+});
+
 class NavBar extends React.Component {
   render() {
     const { messages } = this.props.intl;
-    const coins = [
-      {
-        name: 'ETH',
-        price: '200'
-      },
-      {
-        name: 'BTC',
-        price: '7,000'
-      },
-    ]
+    const { listOfferPrice } = this.props;
+
     return (
       <div className="cash-nav-bar">
-        {/*<button type="button" className="btn bg-transparent mr-2" onClick={() => console.log('clickmenu')}>☰</button>*/}
-        <button className="ml-2">
+        {/* <button type="button" className="btn bg-transparent mr-2" onClick={() => console.log('clickmenu')}>☰</button> */}
+        {/* <button className="ml-2">
           <img src={iconMyATM} width={24} />
           <span className="mx-2">My ATM</span>
-        </button>
+        </button> */}
         <div className="d-inline-block">
           {
-            coins.map(coin => {
-              const { name, price } = coin;
+            listCurrency.map(coin => {
+              const { id, text } = coin;
+              const { price } = getOfferPrice(listOfferPrice, EXCHANGE_ACTION.SELL, id, FIAT_CURRENCY.USD);
               return (
-                <span key={name} className="d-inline-block mr-3">
-                  <img src={iconMap[name]} width={24} />
+                <span key={id} className="d-inline-block mr-3">
+                  {text}
                   {' '}
-                  <span><strong>{price}</strong> USD</span>
+                  <span><strong>{formatMoneyByLocale(price, FIAT_CURRENCY.USD)}</strong> {FIAT_CURRENCY.USD} </span>
                 </span>
-              )
+              );
             })
           }
-          <span>
-
-          </span>
         </div>
       </div>
     );
@@ -76,6 +71,7 @@ class NavBar extends React.Component {
 }
 
 const mapState = state => ({
+  listOfferPrice: state.exchange.listOfferPrice,
 });
 
 const mapDispatch = dispatch => ({
