ed3cee6248b4b68a018aeaaa63de8a54dfd0d52f
2Apple_Q2 <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-10-01 18:48:22 +0700

    Trader List & Trader Detail

--- src/pages/Invest/TraderDetail.jsx
+++ src/pages/Invest/TraderDetail.jsx
@@ -1,7 +1,77 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import InvestNavigation from './InvestNavigation';
+import StarRatings from 'react-star-ratings';
+import './TraderDetail.scss';
+import './TraderList.scss';
+import { green } from 'ansi-colors';
+
+const TraderDetailBlock = ({ rating }) => (
+    <div key={'addlater'} style={{ marginTop: '1em' }} >
+        <div className="profile">
+            <div className="relativeLine">
+                <div className="profile-picture">
+                <img
+                    src={'https://randomuser.me/api/portraits/men/9.jpg'} 
+                />
+                <label>{'Quang Vo'}</label>
+                <div className="star-ratings">
+                    <StarRatings
+                    className="stars"
+                    rating={rating}
+                    isSelectable={false}
+                    starDimension="14px"
+                    starRatedColor="#546FF7"
+                    starSpacing="3px"
+                    numberOfStars={5}
+                    name="rating"
+                    />
+                    <span className="rating-count">(26)</span>
+                </div>
+            </div>
+            </div>
+            <div style={{ height: '50px' }}></div>
+            <div className="profile-banner"></div>
+            <div className="relativeLine">
+                <div className="profile-sumary">
+                    <div className="block block-b1">
+                        <label className="black">{'115'}</label>
+                        <label className="grey">{'ACTIVE PROJECTS'}</label>
+                    </div>
+                    <div className="block block-b2">
+                        <label className="black">{'$200,000,000'}</label>
+                        <label className="grey">{'CUM.VALUE OF MANAGED FUNDS'}</label>
+                    </div>
+                    <div className="block block-b2">
+                        <label className="green">{'32%'}</label>
+                        <label className="grey">{'AVERAGE RETURN'}</label>
+                    </div>
+                    <div className="block block-b1">
+                        <label className="black">{'$15,000'}</label>
+                        <label className="grey">{'CUM EARNINGS'}</label>
+                    </div>
+                </div>
+            </div>
+            <div style={{ height: '200px' }}></div>
+            <div className="funding">
+                <div className="funding-title">
+                    <label>CURRENTLY FUNDING</label>
+                </div>
+                <div className="funding-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+);
 
 export default class TraderDetail extends Component {
     render(){
-        return <div>TraderDetail</div>
+        const item = { rating: 1 }
+        return (
+            <div style={{ backgroundColor: '#fafbff', minHeight: '100vh' }}>
+                <InvestNavigation header="Trader" history={this.props.history} />
+                <TraderDetailBlock {...item} />
+            </div>
+        )
     }
 }
--- src/pages/Invest/TraderList.jsx
+++ src/pages/Invest/TraderList.jsx
@@ -1,8 +1,88 @@
 import React, {Component} from 'react'
 import Web3 from './Web3'
+import ('./TraderList.scss');
+import StarRatings from 'react-star-ratings';
+import ErrorBoundary from '../../components/ErrorBoundary';
+const listTrading = [
+    {
+        id: 1,
+        avatarUrl: 'https://randomuser.me/api/portraits/men/9.jpg',
+        username: 'Booby Gabershek',
+        rating: 1,
+        average: -0.36,
+        amount: 2000,
+    },
+    {
+        id: 2,
+        avatarUrl: 'https://randomuser.me/api/portraits/men/9.jpg',
+        username: 'Quang Vo',
+        rating: 3,
+        average: 0.25,
+        amount: 14567892000
+    },
+    {
+        id: 3,
+        avatarUrl: 'https://randomuser.me/api/portraits/men/9.jpg',
+        username: 'Anonymous',
+        rating: 3,
+        average: 0.2,
+        amount: 1000650
+    }
+];
+
+const TraderItem = ({ avatarUrl, username, rating, average, amount, handleOnClick }) => (
+    <div className="traderItem" onClick={handleOnClick}>
+        <div className="userInfoBlock clearfix">
+            <div className="traderItem-left">
+            <img
+                src={avatarUrl}
+                alt=""
+                className="userImage"
+            />
+            <label>{username}</label>
+            <div className="star-ratings">
+                <StarRatings
+                className="stars"
+                rating={rating}
+                isSelectable={false}
+                starDimension="14px"
+                starRatedColor="#546FF7"
+                starSpacing="3px"
+                numberOfStars={5}
+                name="rating"
+                />
+                <span className="rating-count">(26)</span>
+            </div>
+            </div>
+            <div className="traderItem-right">
+            <div>
+                <label className={ average > 0 ? "green" : "red" }>{`${average*100}%`}</label>
+                <label className="grey fontSmall">AVERAGE RETURNS</label>
+            </div>
+            <div>
+                <label>${amount}</label>
+                <label className="grey fontSmall">CUM EARNINGS</label>
+            </div>
+            </div>
+        </div>
+    </div>
+);
+
+const TraderItemBoudary = (props) => (
+    <ErrorBoundary>
+        <TraderItem {...props} />
+    </ErrorBoundary>
+);
 
 export default class TraderList extends Component {
-    render(){
-        return <div>TraderList</div>
+    
+    navigateToDetail = (item) => {
+        console.log(item);
+        this.props.history.push(`/invest/trader/${item.id}`)
+    }
+    render() {
+        return (
+            listTrading.map((e, i) => (<TraderItemBoudary handleOnClick={this.navigateToDetail.bind(this, e)} key={i} {...e} />))
+        )
     }
 }
