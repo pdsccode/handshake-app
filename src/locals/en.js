d6fe88c19d7bca6898de0b06dbbf2220edd38648
Keresh <vuong@ninja.org>
2018-10-02 17:31:54 +0700

    Update Update ATM screen

--- src/locals/en.js
+++ src/locals/en.js
@@ -1112,6 +1112,8 @@ export default {
         storeAtm: 'Store ATM',
         confirmCreateAtm: 'Create your ATM?',
         confirmUpdateAtm: 'Update your ATM?',
+        createdAtmSuccess: 'Success! You have created an ATM on Ninja.',
+        updatedAtmSuccess: 'Success!',
       },
       button: {
         create: 'Create my ATM',
