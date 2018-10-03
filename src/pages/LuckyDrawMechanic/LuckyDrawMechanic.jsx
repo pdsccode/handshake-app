20d122cb8d98445442d46951a5123763353bbd58
Hakawai <vtkimsa@gmail.com>
2018-10-03 17:33:05 +0700

    Add LuckyDrawMechanic

---
+++ src/pages/LuckyDrawMechanic/LuckyDrawMechanic.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+class LuckyDrawMechanic extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+export default LuckyDrawMechanic;
