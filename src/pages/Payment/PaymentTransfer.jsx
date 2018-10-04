5e1b6efaf9d48e0c7597bc0eadf3a3343d1aa723
Khoa T <khoa.trinh@autonomous.nyc>
2018-10-04 11:46:00 +0700

    update

--- src/pages/Payment/PaymentTransfer.jsx
+++ src/pages/Payment/PaymentTransfer.jsx
@@ -22,19 +22,14 @@ import {
   fieldPhoneInput,
   fieldRadioButton
 } from '@/components/core/form/customField';
-import {required} from '@/components/core/form/validation';
 import {change, Field, formValueSelector, clearFields} from 'redux-form';
 import {bindActionCreators} from 'redux';
 import ModalDialog from '@/components/core/controls/ModalDialog';
 import Modal from '@/components/core/controls/Modal';
-import Dropdown from '@/components/core/controls/Dropdown';
-import createForm from '@/components/core/form/createForm';
 import TransferToken from '@/components/Wallet/TransferToken';
-import { setHeaderRight } from '@/reducers/app/action';
+import Complete from './Complete';
 import { showAlert } from '@/reducers/app/action';
 import { showLoading, hideLoading } from '@/reducers/app/action';
-import local from '@/services/localStore';
-import {APP} from '@/constants';
 import _ from 'lodash';
 import qs from 'querystring';
 
@@ -49,7 +44,9 @@ class PaymentTransfer extends React.Component {
 
     this.state = {
       msgError: '',
-      modalTransfer: ''
+      modalTransfer: '',
+      modalComplete: '',
+      fullBackUrl: ''
     };
   }
 
@@ -60,22 +57,44 @@ class PaymentTransfer extends React.Component {
   async getTokenData() {
     const querystring = window.location.search.replace('?', '');
     this.querystringParsed = qs.parse(querystring);
-    let { to:toAddress, amount, gas_limit:gasLimit , data, confirm_url } = this.querystringParsed;
+    let { to:toAddress, amount, gas_limit:gasLimit , order_id, data, confirm_url } = this.querystringParsed;
 
     if (!toAddress && !amount && !data) {
       this.showModalError("Missing parameters");
       return;
     }
 
-    console.log(toAddress, amount);
+    let fullBackUrl = `${confirm_url}?order_id=${order_id}&status=0`;
     this.setState({
+      fullBackUrl: fullBackUrl,
       modalTransfer: <TransferToken
+        coinName="ETH"
         toAddress={toAddress}
         amount={amount}
         gasLimit={gasLimit}
+        data={data}
+        onFinish={result => { this.successTransfer(result); }}
       />
       }, () => {
         this.modalTransferRef.open();
+      }
+    );
+  }
+
+  cancelTransfer = () => {
+    if(this.state.fullBackUrl && this.state.modalTransfer){
+      window.location.href = this.state.fullBackUrl;
+    }
+  }
+
+  successTransfer = (data) => {
+    this.setState({
+      modalComplete: <Complete
+        data={data}
+      />,
+      modalTransferRef: ''
+      }, () => {
+        this.modalCompleteRef.open();
       }
     );
   }
@@ -89,12 +108,12 @@ class PaymentTransfer extends React.Component {
 
   render = () => {
     const { messages } = this.props.intl;
-    const { modalTransfer } = this.state;
+    const { modalTransfer, modalComplete } = this.state;
 
     return (
       <div className="wallet-transfer">
 
-        <Modal title="Transfer Token" onRef={modal => this.modalTransferRef = modal}  onClose={() => this.closeTransfer()}>
+        <Modal title="Transfer Token" onRef={modal => this.modalTransferRef = modal}  onClose={() => this.cancelTransfer()}>
           {modalTransfer}
         </Modal>
 
@@ -102,9 +121,10 @@ class PaymentTransfer extends React.Component {
           <div className="msg-error">{this.state.msgError}</div>
         </Modal>
 
-        <div style={{width: "100%"}}>
-          Test
-        </div>
+        <ModalDialog className="complete-wrapper" title="Payment complete" onRef={modal => this.modalCompleteRef = modal}>
+          {modalComplete}
+        </ModalDialog>
+
       </div>
     );
   }
