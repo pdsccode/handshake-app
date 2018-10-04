d3b866cd4f609ae357e543fb78bec4fcb7783490
Tatsu <nghia@autonomous.com>
2018-10-04 18:13:34 +0700

    .

--- serve.js
+++ serve.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const path = require('path');
 const compression = require('compression');
 const fallback = require('express-history-api-fallback');
 
 const app = express();
 
 const root = `${__dirname}/dist`;
-app.set('view engine', 'html');
 app.use(express.static(root));
 app.use(fallback('index.html', { root }));
 app.use(compression());
 app.get('*', (req, res) => {
-  res.render('index');
+  res.sendFile(`${root}/index.html`);
 });
 
 const port = process.env.PORT || 8080;
