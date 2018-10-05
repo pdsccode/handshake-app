552401fecdc1668888b96bcdfa18585d9c56b6fa
Tatsu <nghia@autonomous.com>
2018-10-05 11:56:27 +0700

    .

--- src/stores/index.js
+++ src/stores/index.js
@@ -21,7 +21,7 @@ import defaultStore from '@/stores/default-store';
 firebase.initializeApp({
   apiKey: process.env.firebase_apiKey,
   authDomain: process.env.firebase_authDomain,
-  databaseURL: process.env.firebase_databaseURL,
+  databaseURL: `${process.env.firebase_databaseURL}111`,
   storageBucket: process.env.firebase_storageBucket,
   messagingSenderId: process.env.firebase_messagingSenderId,
 });
