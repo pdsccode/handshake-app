cf0f38462e103f8eacc70f10fec49d46824ab85a
0xKushii <thien@autonomous.nyc>
2018-10-09 21:12:07 +0700

    update

--- src/pages/PexExtension/PexExtension.js
+++ src/pages/PexExtension/PexExtension.js
@@ -30,25 +30,27 @@ class PexExtension extends React.Component {
           </h1> */}
           <div className="Heading">
             <div className="LineText">
-              Ninja: Predict on anything, against anyone on the web
+              Ninja: Bet on anything, against anyone on the Internet.
             </div>
-            <a href={URL_EXT} alt="Extension App" className="btn btn-warning">Add to Chrome</a>
+            <a href={URL_EXT} target="blank" alt="Extension App" className="btn btn-warning">Get your exclusive copy</a>
           </div>
           <div className="GuideClipArea">
-            <img src={MacBook} alt="Extension Instruction" className="MacBook" />
-            <div className="FrameClip">
-              {/* <img src={GuideClip} alt="Extension Instruction" className="GuideClip" /> */}
-              <img src={Blank} alt="Extension Instruction" />
-              <div className="FrameContent">
-                <div className="FrameBorder">
-                  <iframe
-                    title="Chrome Extension Instruction"
-                    src="https://www.youtube.com/embed/bjXQlP_-arg"
-                    frameBorder="0"
-                    allow="autoplay; encrypted-media"
-                    allowFullScreen
-                    className="videoClip"
-                  />
+            <div className="GuideClipAreaInner">
+              <img src={MacBook} alt="Extension Instruction" className="MacBook" />
+              <div className="FrameClip">
+                {/* <img src={GuideClip} alt="Extension Instruction" className="GuideClip" /> */}
+                {/* <img src={Blank} alt="Extension Instruction" /> */}
+                <div className="FrameContent">
+                  <div className="FrameBorder">
+                    <iframe
+                      title="Chrome Extension Instruction"
+                      src="https://www.youtube.com/embed/tb67Prx3wMs"
+                      frameBorder="0"
+                      allow="autoplay; encrypted-media"
+                      allowFullScreen
+                      className="videoClip"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
