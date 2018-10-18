af8dd42a888c79d83ac0d8f9b27d6570f71985ae
0xmegalodon <dung.thanh@autonomous.nyc>
2018-10-18 17:08:15 +0700

    revert

--- src/pages/InternalAdmin/InternalAdmin.jsx
+++ src/pages/InternalAdmin/InternalAdmin.jsx
@@ -337,7 +337,7 @@ class InternalAdmin extends Component {
         return this.getStatus(row);
       },
     }, {
-      dataField: 'status',
+      dataField: 'action',
       text: 'Action',
       formatter: (cell, row, rowIndex, formatExtraData) => {
         const order = orderList[rowIndex];
