db6fb79f5406c3e21d45cda41c721991594857e3
0xmegalodon <dung.thanh@autonomous.nyc>
2018-10-24 08:44:29 +0700

    add coin faq page

--- src/constants.js
+++ src/constants.js
@@ -639,6 +639,7 @@ export const URL = {
   CC_PAYMENT_URL: '/cc-payment',
   BUY_BY_CC_URL: '/buy-by-credit-card',
   BUY_COIN_URL: '/coin',
+  BUY_COIN_FAQ_URL: '/coin/faq',
 
   ESCROW_WITHDRAW_SUCCESS: '/escrow/withdraw/success',
 
